3b3bf46b0e6e7d923942f9f90ec4fee0b42ba9ff
= <abiemarshall94@gmail.com>
2020-08-10 22:55:35 +0100

    rest api can be queried for telemetry data

--- api/api.py
+++ api/api.py
@@ -1,10 +1,21 @@
-import time
+import socket
 
 from flask import Flask
+
+from forzaDataPacket import ForzaDataPacket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind(('', 5685))
+params = ForzaDataPacket.get_props(packet_format='fh4')
 
 app = Flask(__name__)
 
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+@app.route('/telemetry')
+def get_telemetry_packet():
+    telemetry = None
+    while not telemetry:
+        message, address = server_socket.recvfrom(1024)
+        telemetry = ForzaDataPacket(message, packet_format='fh4')
+
+    return telemetry.to_dict(params)
