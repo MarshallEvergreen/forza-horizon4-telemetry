af08cfdbcc076e0555647669f77fa25d2acbf31e
= <abiemarshall94@gmail.com>
2020-08-11 20:14:18 +0100

    Updated index

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import NavBar from "./navBar";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <NavBar/>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
