46a2bd7c4544dc1f3ccd1b74388206c545defdb3
= <abiemarshall94@gmail.com>
2020-08-11 20:11:17 +0100

    changed navbar color

--- react-app/src/navBar.js
+++ react-app/src/navBar.js
@@ -1,29 +1,22 @@
 import React from "react";
 import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import ShoppingBasket from "@material-ui/icons/ShoppingBasket";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import AppBar from "@material-ui/core/AppBar";
+import teal from '@material-ui/core/colors/teal';
 
 class NavBar extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {date: 'unknown'};
     }
 
     render() {
         return (
             <div>
-                <AppBar position="static">
+                <AppBar style={{backgroundColor: teal[500]}} position="static">
                     <Toolbar>
-                        <IconButton edge="start" color="inherit" aria-label="menu">
-                            <ShoppingBasket/>
-                        </IconButton>
                         <Typography variant="h6">
-                            News
+                            Telemetry
                         </Typography>
-                        <Button color="inherit">Login</Button>
                     </Toolbar>
                 </AppBar>
             </div>
