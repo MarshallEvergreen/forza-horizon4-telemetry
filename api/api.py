16d0322607eea660a0f4d29f80fef7ff8b28ecee
= <abiemarshall94@gmail.com>
2020-08-15 11:08:39 +0100

    added socket io to api

--- api/api.py
+++ api/api.py
@@ -1,6 +1,7 @@
 import socket
 
 from flask import Flask
+from flask_socketio import SocketIO, emit
 
 from forzaDataPacket import ForzaDataPacket
 
@@ -9,6 +10,8 @@ server_socket.bind(('', 5685))
 params = ForzaDataPacket.get_props(packet_format='fh4')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 
 @app.route('/telemetry')
@@ -16,3 +19,14 @@ def get_telemetry_packet():
     message, address = server_socket.recvfrom(1024)
     telemetry = ForzaDataPacket(message, packet_format='fh4')
     return telemetry.to_dict(params)
+
+
+@socketio.on('request telemetry')
+def test_message(message):
+    message, address = server_socket.recvfrom(1024)
+    telemetry = ForzaDataPacket(message, packet_format='fh4')
+    emit('telemetry response', telemetry.to_dict(params), broadcast=True)
+
+
+if __name__ == '__main__':
+    socketio.run(app)
